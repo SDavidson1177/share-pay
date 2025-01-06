12fa4a38f9ce75304d2ccb79252adb9ba03ad44a
SDavidson1177 <SDavidson1177@gmail.com>
2025-01-05 20:07:12 -0500

    Able to add participants

--- src/SharePay.sol
+++ src/SharePay.sol
@@ -7,10 +7,12 @@ contract SharePay {
         string title;
         uint amount;
         address[] participants;
+        address[] requests;
     }
 
     address private _owner;
     Bill[] private _bills;
+
 
     constructor() {
         _owner = msg.sender;
@@ -20,9 +22,11 @@ contract SharePay {
         return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
     }
 
+    /* BILL MANIPULATIONS */
+
     // Create a null bill
     function nullBill() public view returns (Bill memory) {
-        return Bill(msg.sender, "", 0, new address[](0));
+        return Bill(msg.sender, "", 0, new address[](0), new address[](0));
     }
 
     // Check if a bill is null
@@ -32,18 +36,40 @@ contract SharePay {
 
     // Create a bill
     function createBill(string memory _title, uint _amount) public {
-        _bills.push(Bill({owner: msg.sender, title: _title, amount: _amount, participants: new address[](0)}));
+        _bills.push(Bill({owner: msg.sender, title: _title, amount: _amount, participants: new address[](0), requests: new address[](0)}));
     }
 
     // Get a bill
-    function getBill(string memory _title) public view returns (Bill memory) {
+    function getBill(string memory _title) public view returns (Bill memory, uint) {
         for (uint i = 0; i < _bills.length; i++) {
             if (areStringsEqual(_bills[i].title, _title)) {
-                return _bills[i];
+                return (_bills[i], i);
             }
         }
 
-        return nullBill();
+        return (nullBill(), 0);
+    }
+
+    /* PARTICIPANT INTERACTIONS */
+
+    // Request to join a bill
+    function requestToJoin(string memory _title) public {
+        (Bill memory req_bill, uint index) = getBill(_title);
+        require(!isBillNull(req_bill));
+
+        _bills[index].requests.push(msg.sender);
+    }
+
+    function acceptRequest(string memory _title, address requester) public {
+        (Bill memory req_bill, uint index) = getBill(_title);
+        require(!isBillNull(req_bill));
+        require(req_bill.owner == msg.sender);
+
+        for (uint i = 0; i < req_bill.requests.length; i++) {
+            if (req_bill.requests[i] == requester) {
+                _bills[index].participants.push(requester);
+            }
+        }
     }
 
 }
--- test/SharePay.t.sol
+++ test/SharePay.t.sol
@@ -13,8 +13,40 @@ contract SharePayTest is Test {
 
     function test_CreateBill() public {
         pay.createBill("test_bill", 100);
-        SharePay.Bill memory b = pay.getBill("test_bill");
+        (SharePay.Bill memory b, ) = pay.getBill("test_bill");
         assertEq(b.title, "test_bill");
         assertEq(b.amount, 100);
+
+        // failure cases
+        (SharePay.Bill memory c, ) = pay.getBill("null_bill");
+        assert(pay.isBillNull(c));
+        assert(!pay.isBillNull(b));
+    }
+
+    function test_AddParticipant() public {
+        string memory bill_name = "test_bill";
+
+        // create bill
+        vm.prank(address(0));
+        pay.createBill(bill_name, 100);
+        (SharePay.Bill memory b, ) = pay.getBill(bill_name);
+        assertEq(b.requests.length, 0);
+
+        // request participation
+        vm.prank(address(1));
+        pay.requestToJoin(bill_name);
+        (b, ) = pay.getBill(bill_name);
+        assertEq(b.requests.length, 1);
+        assertEq(b.participants.length, 0);
+
+        // failure case (non-owner)
+        vm.expectRevert();
+        pay.acceptRequest(bill_name, address(1));
+
+        // accept participation
+        vm.prank(address(0));
+        pay.acceptRequest(bill_name, address(1));
+        (b, ) = pay.getBill(bill_name);
+        assertEq(b.participants.length, 1);
     }
 }
