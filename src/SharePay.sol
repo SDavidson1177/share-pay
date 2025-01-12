ad0a892fb15fb4b8d73196c9138dd81cde5121ce
SDavidson1177 <SDavidson1177@gmail.com>
2025-01-12 11:40:08 -0500

    deposit and withdraw functions

--- src/SharePay.sol
+++ src/SharePay.sol
@@ -12,18 +12,26 @@ contract SharePay {
 
     address private _owner;
     mapping(address => mapping(string => Bill)) private _bills;
-    // Bill[] private _bills;
-
+    mapping(address => uint) private _balances;
 
     constructor() {
         _owner = msg.sender;
+    }
+
+    receive() external payable {}
+
+    modifier billExists(address _bill_owner, string memory _title) {
+        Bill memory b = _bills[_bill_owner][_title];
+        assert(!isBillNull(b));
+
+        _;
     }
 
     function areStringsEqual(string memory a, string memory b) private pure returns (bool) {
         return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
     }
 
-    /* BILL MANIPULATIONS */
+    /* Bill Manipulations */
 
     // Create a null bill
     function nullBill() public view returns (Bill memory) {
@@ -48,18 +56,15 @@ contract SharePay {
         return b;
     }
 
-    /* PARTICIPANT INTERACTIONS */
+    /* Participant Interactions */
 
     // Request to join a bill
-    function requestToJoin(address _bill_owner, string memory _title) public {
-        // check that bill exists
-        getBill(_bill_owner, _title);
-
+    function requestToJoin(address _bill_owner, string memory _title) public billExists(_bill_owner, _title) {
         _bills[_bill_owner][_title].requests.push(msg.sender);
     }
 
     function acceptRequest(string memory _title, address requester) public {
-        // check that bill exists
+        // Get the bill
         Bill memory b = getBill(msg.sender, _title);
 
         for (uint i = 0; i < b.requests.length; i++) {
@@ -69,4 +74,32 @@ contract SharePay {
         }
     }
 
+    // Participant deposits funds
+    function deposit() public payable {
+        _balances[msg.sender] += msg.value;
+    }
+
+    // Bill owner accepts payment for a bill
+    function acceptPayment(string memory _title) public payable {
+        Bill memory b = getBill(msg.sender, _title);
+        assert(b.owner == msg.sender);
+
+        // payable(msg.sender).transfer(10 ether);
+
+        uint amount_payable = b.amount / (b.participants.length + 1);
+        uint remainder = b.amount % (b.participants.length + 1);
+
+        for (uint i = 0; i < b.participants.length; i++) {
+            uint rem = 0;
+            if (remainder > 0) {
+                rem++;
+                remainder--;
+            }
+
+            assert(_balances[b.participants[i]] >= amount_payable + rem);
+            _balances[b.participants[i]] -= amount_payable + rem;
+            payable(b.owner).transfer(amount_payable + rem);
+        }
+
+    }
 }
--- test/SharePay.t.sol
+++ test/SharePay.t.sol
@@ -11,6 +11,16 @@ contract SharePayTest is Test {
         pay = new SharePay();
     }
 
+    function establishParticipants(address _owner, string memory _title, address[] memory participants) private {
+        for (uint i = 0; i < participants.length; i++) {
+            vm.prank(participants[i]);
+            pay.requestToJoin(_owner, _title);
+
+            vm.prank(_owner);
+            pay.acceptRequest(_title, participants[i]);
+        }
+    }
+
     function test_CreateBill() public {
         vm.prank(address(0));
         pay.createBill("test_bill", 100);
@@ -50,5 +60,42 @@ contract SharePayTest is Test {
         pay.acceptRequest(bill_name, address(1));
         b = pay.getBill(owner, bill_name);
         assertEq(b.participants.length, 1);
+    }
+
+    function test_Withdrawls() public {
+        address owner = address(0);
+        string memory _bill_name = "test_bill";
+        address[] memory participants = new address[](4);
+        for (uint160 i = 0; i < 4; i++) {
+            participants[i] = address(i+1);
+        }
+        
+        // Fund accounts
+        vm.prank(owner);
+        pay.createBill(_bill_name, 100 ether);
+        vm.deal(owner, 1000 ether);
+        for (uint i = 0; i < 4; i++) {
+            vm.deal(participants[i], 1000 ether);
+        }
+
+        // Add participants
+        establishParticipants(owner, _bill_name, participants);
+
+        // Participants deposit
+        for (uint i = 0; i < 4; i++) {
+            vm.prank(participants[i]);
+            pay.deposit{value: 20 ether}();
+        }
+        assertEq(address(pay).balance, 80 ether);
+
+        // Owner Accepts payment
+        vm.prank(owner);
+        pay.acceptPayment(_bill_name);
+        for (uint i = 0; i < 4; i++) {
+            assertEq(participants[i].balance, 980 ether);
+        }
+
+        assertEq(owner.balance, 1080 ether);
+        assertEq(address(pay).balance, 0 ether);
     }
 }
